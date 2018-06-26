15ba9a3507eeed01a0ad464edacb73c03b8743c6
H.Kiani <kiani.hamidreza@gmail.com>
2018-06-26 20:35:44 +0430

    Add Salary and Report menu to main form .

--- SalaryApp/SalaryApp/SalaryApp.WinClient/MainForm.cs
+++ SalaryApp/SalaryApp/SalaryApp.WinClient/MainForm.cs
@@ -12,6 +12,8 @@ namespace SalaryApp.WinClient
             this.WindowState=FormWindowState.Maximized;
             this.Text = @"برنامه حقوق و دستمزد شرکت یاور پویای جنوب";
 
+
+            //base info menu
             var baseInfoItem =(ToolStripMenuItem) MainMenuStrip.Items.Add("اطلاعات پایه",null,null);
 
             baseInfoItem.DropDownItems.Add("اطلاعات شرکت ها", null, (obj, e) =>
@@ -34,6 +36,26 @@ namespace SalaryApp.WinClient
 
 
             baseInfoItem.DropDownItems.Add("خروج", null, (obj, e) => { Application.Exit();});
+
+
+            //salary menu
+            var salaryMenu =(ToolStripMenuItem) MainMenuStrip.Items.Add("حقوق و دستمزد", null);
+
+            salaryMenu.DropDownItems.Add("ایجاد ماه جدید", null, null);
+
+            
+            //report Menu
+            var reportMenu = (ToolStripMenuItem) MainMenuStrip.Items.Add("گزارشات", null);
+
+            reportMenu.DropDownItems.Add("بیمه", null, null);
+            reportMenu.DropDownItems.Add("مالیات", null, null);
+            reportMenu.DropDownItems.Add("مساعده", null, null);
+            reportMenu.DropDownItems.Add("حضور و غیاب", null, null);
+            reportMenu.DropDownItems.Add("لیست حقوق", null, null);
+            reportMenu.DropDownItems.Add("لیست بانک", null, null);
+            reportMenu.DropDownItems.Add("پاداش بهره وری", null, null);
+
+
 
         }
     }
